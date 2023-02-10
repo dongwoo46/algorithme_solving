03db0db8338ef5a89d33eb2c761c7c419bcd62a2
김동우 <qwa406@naver.com>
2023-02-10 16:57:59 +0900

    ãeditor_using_slice

--- 백준/Silver/1406. 에디터/에디터.py
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -20,3 +20,53 @@ for i in range(n):
             str1.pop()
 str1.extend(reversed(str2))
 print(''.join(str1))
+
+
+'''
+##리스트 슬라이싱 문제풀이
+import sys
+sys.stdin = open('input','r')
+
+s = input()
+str = s + '*'
+
+n = int(sys.stdin.readline().rstrip())
+
+for i in range(n):
+    order = sys.stdin.readline().split()
+    idx = str.index('*')
+    if order[0] == 'P':
+
+        if str[-1] == '*':
+            str = str[:idx] + order[1] + '*'
+        elif idx == 0:
+            str = order[1] + '*' + str[idx+1:]
+        else:
+            str = str[:idx]+order[1]+'*'+str[idx+1:]
+    elif order[0] == 'L':
+        if idx == 0:
+            continue
+        elif str[-1] == '*':
+            str = str[:idx-1]+'*'+str[-2]
+        else:
+            str = str[:idx-1] + '*' + str[idx-1]+str[idx+1:]
+    elif order[0] == 'D':
+        if str[-1] == '*':
+            continue
+        elif idx == 0:
+            str = str[:idx+1] + '*' + str[idx+2:]
+        else:
+            str = str[:idx]+str[idx+1]+'*'+ str[idx+2:]
+    else:
+        if idx == 0:
+            continue
+        elif str[-1] == '*':
+            str = str[:idx-1]+'*'
+        else:
+            str = str[:idx-1]+'*'+str[idx+1:]
+
+
+str_ans = str.replace('*','')
+for alpha in str_ans:
+    print(alpha, end='')
+'''
