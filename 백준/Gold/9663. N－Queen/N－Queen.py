2bb5356eb39303dcb16f29c087ab41e34715bb8d
siwol <110177263+dongwoo46@users.noreply.github.com>
2023-03-13 23:44:23 +0900

    [Gold IV] Title: N-Queen, Time: 6648 ms, Memory: 125152 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,27 +1,19 @@
-import sys
-n = int(sys.stdin.readline().rstrip())
-
-ans = 0
-row = [0] * n
-
-def is_promising(x):
-    for i in range(x):
-        if row[x] == row[i] or abs(row[x] - row[i]) == abs(x - i):
-            return False
-    
-    return True
-
-def n_queens(x):
+def n_queens(i): # i는 행번호
     global ans
-    if x == n:
+    if i == n:
         ans += 1
-
+        return
     else:
-        for i in range(n):
-            # [x, i]에 퀸을 놓겠다.
-            row[x] = i
-            if is_promising(x):
-                n_queens(x+1)
+        for j in range(n):
+            # 행 렬 대각선방향에 이미 들어간 게 있는지 없는지 체크
+            if v1[j]==v2[j+i]==v3[j-i]==0:
+                v1[j] = v2[j + i] = v3[j - i] = 1
+                n_queens(i+1)
+                v1[j] = v2[j + i] = v3[j- i] = 0
 
+
+n= int(input())
+ans = 0
+v1,v2,v3 = [[0]*(2*n) for _ in range(3)]
 n_queens(0)
 print(ans)
