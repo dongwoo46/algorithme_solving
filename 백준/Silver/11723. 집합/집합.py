82efd8ceffa4e4deb5d13815e4f539390d3eaebd
siwol <110177263+dongwoo46@users.noreply.github.com>
2023-03-25 23:45:24 +0900

    [Silver V] Title: 집합, Time: 3268 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,27 +1,28 @@
 import sys
-m = int(input())
-s = 0b0
 
-for _ in range(m):
-  cmd = list(map(str, sys.stdin.readline().split()))
-  if len(cmd) == 2:
-    n = int(cmd[1])
-    cmd = cmd[0]
-  else:
-    cmd = cmd[0]
-
-  if cmd == "check":
-    if s & (1 << n): # s and n
-      print(1)
-    else: # ~(s and n)
-      print(0)
-  if cmd == "add":
-    s = s | (1 << n) # s or n
-  if cmd == "remove":
-    s = s & ~(1 << n) # s or ~n
-  if cmd == "toggle":
-    s = s ^ (1 << n) # s xor n
-  if cmd == "all":
-    s = (1 << 21) - 1
-  if cmd == "empty":
-    s = 0b0
+input = sys.stdin.readline
+s = 0
+n = int(input())
+for _ in range(n):
+    order = input().split()
+    if order[0] == "add":
+        s = s|(1<<int(order[1]))
+        continue
+    if order[0] == "remove":
+        s = s & ~(1<<int(order[1]))
+        continue
+    if order[0] == "check":
+        if s&(1<<int(order[1])):
+            print(1)
+        else:
+            print(0)
+        continue
+    if order[0] == "toggle":
+        s = s^(1<<int(order[1]))
+        continue
+    if order[0] == "empty":
+        s = 0
+        continue
+    if order[0] == "all":
+        s = (1<<21)-1
+        continue
