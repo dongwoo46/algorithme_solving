7b6a9d1377f266c36275117ce63e5960eafb18d8
siwol <110177263+dongwoo46@users.noreply.github.com>
2023-04-23 16:35:38 +0900

    [Silver I] Title: IOIOI, Time: 116 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -0,0 +1,16 @@
+
+n = int(input())
+m = int(input())
+s = input()
+
+p = 'IOI'+(n-1)*'OI'
+cnt = 0
+
+end = len(p)-1
+for start in range(m-len(p)+1):
+
+    if s[start:end+1] == p:
+        cnt+=1
+    end +=1
+
+print(cnt)
