3117e49a32a0daf26b27daab8232ce96c0f81649
siwol <110177263+dongwoo46@users.noreply.github.com>
2023-07-03 16:05:40 +0900

    [Silver II] Title: 스타트와 링크, Time: 6572 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,27 +1,34 @@
 import sys
-n = int(sys.stdin.readline())
-graph = [ list(map(int, sys.stdin.readline().split())) for _ in range(n) ]
-visit = [ False for _ in range(n) ] #1
-min_value = sys.maxsize #2
 
-def backTracking(depth, idx): #3
-    global min_value
-    if depth == n // 2: #4
+
+input = sys.stdin.readline
+
+n = int(input())
+board = [list(map(int,input().split())) for _ in range(n)]
+visited = [0 for _ in range(n)]
+minnum = 1e9
+
+def backtracking(v,idx):
+    global minnum
+    if v == n//2:
         power1, power2 = 0, 0
         for i in range(n):
             for j in range(n):
-                if visit[i] and visit[j]: #5
-                    power1 += graph[i][j]
-                elif not visit[i] and not visit[j]: #6
-                    power2 += graph[i][j]
-        min_value = min(min_value, abs(power1-power2)) #7
+                if visited[i] and visited[j]:
+                    power1 += board[i][j]
+                elif visited[i] == 0 and visited[j] == 0:
+                    power2 += board[i][j]
+
+        minnum = min(minnum,abs(power2-power1))
         return
 
-    for i in range(idx, n): #8
-        if not visit[i]:
-            visit[i] = True
-            backTracking(depth+1, i+1) #9
-            visit[i] = False
-backTracking(0, 0)
-print(min_value)
- 
+
+    for i in range(idx, n):
+        if visited[i] == 0:
+            visited[i] = 1
+            backtracking(v+1,i+1)
+            visited[i] =0
+
+backtracking(0,0)
+print(minnum)
+
