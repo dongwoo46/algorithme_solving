0f0f16685a3ff4c2ad294595ef34a3bf3757b968
siwol <110177263+dongwoo46@users.noreply.github.com>
2023-07-07 13:46:16 +0900

    [Silver I] Title: 정수 삼각형, Time: 164 ms, Memory: 116020 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,15 @@
+
+# 자기보다 1크거나 자기자신과 index가 같은 것만 고를 수 있음
+n = int(input())
+dp = [list(map(int,input().split())) for _ in range(n)]
+
+for i in range(1, n):
+    for j in range(i+1):
+        if j == 0:
+            dp[i][j] = dp[i][j]+dp[i-1][j]
+        elif j == i:
+            dp[i][j] = dp[i][j] +dp[i-1][j-1]
+        else:
+            dp[i][j] += max(dp[i-1][j],dp[i-1][j-1])
+
+print(max(dp[n-1]))
