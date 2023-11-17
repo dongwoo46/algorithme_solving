8fd7233e066dee858ce5db5b793e59181e746211
김동우 <110177263+dongwoo46@users.noreply.github.com>
2023-11-17 15:47:25 +0900

    [Bronze V] Title: 나머지, Time: 208 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        int first = (A + B) % C;
+        int second = ((A % C) + (B % C)) % C;
+        int third = (A*B)%C;
+        int fourth = ((A % C) * (B % C)) % C;
+
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+        System.out.println(fourth);
+        
+        sc.close();
+    }
+}
